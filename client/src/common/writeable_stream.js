0b5f0dc7ed5b35cf75ddbb1326931ac3a82b615f
Alan Languirand <alan.languirand@gm.com>
2017-07-08 06:00:58 -0400

    writeable_stream

--- client/src/common/writeable_stream.js
+++ client/src/common/writeable_stream.js
@@ -6,21 +6,15 @@ module.exports = function(options, cb) {
 
 function WriteableStream(options, writeCallback) {
   // The amount of data potentially buffered depends on the highWaterMark option passed into the streams constructor. For normal streams, the highWaterMark option specifies a total number of bytes.For streams operating in object mode, the highWaterMark specifies a total number of objects.
-  let defaults = {
-    objectMode: true, // always true. Changes will be ignored
-    highWaterMark: 20 // objects to buffer
-  }
-  let params = Object.assign(defaults, options);
-
   let Buffer = [];
-  let state = new WriteableState(params);
+  let state = new WriteableState(options);
 
   return {
     write: write,
     push: write,
     // read
-    // cork
-    // uncork
+    cork: cork,
+    uncork: uncork,
     // end
     // destroy
     _writableState: state
@@ -35,12 +29,24 @@ function WriteableStream(options, writeCallback) {
     Buffer = [];
   }
 
+  function cork() {
+    state.corked = true;
+  }
+
+  function uncork() {
+    if (state.corked && state.getBuffer().length > 0) {
+      flush();
+      announce('drain');
+    }
+    state.corked = false;
+  }
+
   function write(obj) {
     // Data is buffered in Readable streams when the implementation calls stream.push(chunk).If the consumer of the Stream does not call stream.read(), the data will sit in the internal queue until it is consumed.
 
     // Data is buffered in Writable streams when the writable.write(chunk) method is called repeatedly. While the total size of the internal write buffer is below the threshold set by highWaterMark, calls to writable.write() will return true.Once the size of the internal buffer reaches or exceeds the highWaterMark, false will be returned.
     Buffer.push(obj);
-    if (!writeableState.corked) {
+    if (!state.corked) {
       setTimeout(flush)
     }
     state.hasCapacity = Buffer.length <= state.highWaterMark;
@@ -48,6 +54,12 @@ function WriteableStream(options, writeCallback) {
   }
 
   function WriteableState(options) {
+    let defaults = {
+      objectMode: true, // always true. Changes will be ignored
+      highWaterMark: 100 // objects to buffer
+    }
+    let params = Object.assign(defaults, options);
+
     let isCorked = false;
     let hasCapacity = true;
     return {
@@ -55,24 +67,24 @@ function WriteableStream(options, writeCallback) {
         return isCorked;
       },
       set corked(val) {
-        if (val && corked === false) {
-          flush();
+        if (val && isCorked === false) {
           isCorked = true;
-        } else {
+        }
+        if (!val) {
           isCorked = false;
         }
       },
       get highWaterMark() {
-        return options.highWaterMark;
+        return params.highWaterMark;
       },
       get hasCapacity() {
         return hasCapacity;
       },
       set hasCapacity(val) {
         if (val && hasCapacity === false) {
-          announce('drain');
           hasCapacity = true;
-        } else {
+        }
+        if (!val) {
           hasCapacity = false;
         }
       },
--- client/tests/writeableStream.spec.js
+++ client/tests/writeableStream.spec.js
@@ -1,35 +1,45 @@
-describe('WriteableStream', function(){
+describe('WriteableStream', function() {
   let writeable;
-  beforeAll(function(){
+  beforeAll(function() {
     writeable = require('../src/common/writeable_stream');
   });
 
-  describe('signature', function(){
+  describe('signature', function() {
     let writer;
-    beforeEach(function(){
-      writer = writeable({}, function(){});
+    beforeEach(function() {
+      writer = writeable({}, function() {});
     });
-    it('should expose a write method', function(){
+    it('should expose a write method', function() {
       expect(writer.write).toBeDefined();
       expect(writer.write).toEqual(jasmine.any(Function));
     });
 
-    it('should expose a push method', function(){
+    it('should expose a push method', function() {
       expect(writer.push).toBeDefined();
       expect(writer.push).toEqual(jasmine.any(Function));
     });
-    describe('_writeableState', function(){
-      it('should expose a _writableState utility method', function(){
+
+    it('should expose a cork method', function() {
+      expect(writer.cork).toBeDefined();
+      expect(writer.cork).toEqual(jasmine.any(Function));
+    });
+
+    it('should expose a uncork method', function() {
+      expect(writer.uncork).toBeDefined();
+      expect(writer.uncork).toEqual(jasmine.any(Function));
+    });
+    describe('_writeableState', function() {
+      it('should expose a _writableState utility method', function() {
         expect(writer._writableState).toBeDefined();
         expect(writer._writableState).toEqual(jasmine.any(Object));
       });
-      it('should be uncorked by default', function(){
+      it('should be uncorked by default', function() {
         expect(writer._writableState.corked).toBe(false);
       });
-      it('should have capacity by default', function(){
+      it('should have capacity by default', function() {
         expect(writer._writableState.hasCapacity).toBe(true);
       });
-      it('should have an empty buffer by default', function(){
+      it('should have an empty buffer by default', function() {
         let buffer = writer._writableState.getBuffer();
         expect(buffer).toEqual(jasmine.any(Array));
         expect(buffer.length).toEqual(0);
@@ -37,10 +47,102 @@ describe('WriteableStream', function(){
     });
   });
 
-  describe('write method', function(){
-    let writer;
-    beforeEach(function(){
-      writer = writeable({}, function(){});
+  describe('init', function() {
+    it('should init with a default hwm of 100', function(){
+      let writer = writeable({}, function() {});
+      expect(writer._writableState.highWaterMark).toEqual(100);
+    });
+    it('can override high water mark at init', function(){
+      let writer = writeable({highWaterMark: 42}, function() {});
+      expect(writer._writableState.highWaterMark).toEqual(42);
+    });
+    it('high water mark readonly AFTER init', function(){
+      let writer = writeable({highWaterMark: 42}, function() {});
+      writer._writableState.highWaterMark = 100;
+      expect(writer._writableState.highWaterMark).toEqual(42);
+    });
+  });
+  describe('write method', function() {
+    it('should stream chunks into write function', function(done) {
+      let tracer = jasmine.createSpy('writeCb', function(){});
+      let writer = writeable({highWaterMark: 42}, tracer);
+      writer.write('hi mom');
+      setTimeout(function(){
+        expect(tracer.calls.any()).toBe(true);
+        done();
+      });
+    });
+
+    it('should wait to flush on next tick', function(done) {
+      let tracer = jasmine.createSpy('writeCb', function(){});
+      let writer = writeable({highWaterMark: 42}, tracer);
+      writer.write('I love lamp');
+      writer.write('hes got a spear');
+      writer.write('stay classy san francisco');
+
+      expect(writer._writableState.getBuffer().length).toEqual(3);
+
+      setTimeout(function(){
+        expect(writer._writableState.getBuffer().length).toEqual(0);
+        done();
+      });
+    });
+
+    it('push should be an alias for write', function() {
+      let tracer = jasmine.createSpy('writeCb', function(){});
+      let writer = writeable({highWaterMark: 42}, tracer);
+      expect(writer.write).toEqual(writer.push);
+    });
+
+    it('should exert backpressure based on highWaterMark', function() {
+      let tracer = jasmine.createSpy('writeCb', function(){});
+      let writer = writeable({highWaterMark: 5}, tracer);
+      'backpressure'.split().forEach(function(val, i){
+        let hasCapcity = writer.write(val);
+        if (i > writer._writableState.highWaterMark) {
+          expect(hasCapcity).toBe(false);
+        } else {
+          expect(hasCapcity).toBe(true);
+        }
+      })
+
+    })
+  });
+
+  describe('corking', function() {
+    it('toggling corking without writing should not flush', function(done) {
+      let tracer = jasmine.createSpy('writeCb', function(){});
+      let writer = writeable({highWaterMark: 42}, tracer);
+      writer.cork();
+      writer.uncork();
+      setTimeout(function(){
+        expect(tracer.calls.any()).toBe(false);
+        done();
+      });
+    });
+
+    it('corked streams should not flush', function(done) {
+      let tracer = jasmine.createSpy('writeCb', function(){});
+      let writer = writeable({highWaterMark: 42}, tracer);
+      writer.cork();
+      writer.write('hi mom');
+      setTimeout(function(){
+        expect(tracer.calls.any()).toBe(false);
+        done();
+      });
+    });
+
+    it('uncorking stream will immediately flush', function(done) {
+      let tracer = jasmine.createSpy('writeCb', function(){});
+      let writer = writeable({highWaterMark: 42}, tracer);
+      writer.cork();
+      writer.write('hi mom');
+      setTimeout(function(){
+        expect(tracer.calls.any()).toBe(false);
+        writer.uncork();
+        expect(tracer.calls.any()).toBe(true);
+        done();
+      });
     });
   });
 });
